648197c9c48e3fe3df0eeaa31eb951466866b3c8
Facundo Duart <busquedad@github.com>
2020-07-02 21:07:58 -0300

    se verifica compilacion en Sumsung S10/Motorola moto G4

--- TestMotoG4API24/app/src/main/cpp/native-lib.cpp
+++ TestMotoG4API24/app/src/main/cpp/native-lib.cpp
@@ -2,9 +2,9 @@
 #include <string>
 
 extern "C" JNIEXPORT jstring JNICALL
-Java_com_example_androidndksetuptestii_MainActivity_stringFromJNI(
+Java_com_example_holamundo_MainActivity_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
-    std::string hello = "debuggin on samsung s10 device";
+    std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
 }
